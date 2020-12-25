dcf6c60c302ea1ef62de413606092eee087a6e93
Alexey Ryabtsev <61757573+chicagomg@users.noreply.github.com>
2020-12-26 00:31:05 +0300

    Update button.sh

--- button.sh
+++ button.sh
@@ -8,7 +8,7 @@ fi
 read -p "Press enter to continue"
 
 if [ "$(git version | cut -c 1-11)" = "git version" ];then
-    cd ~ && /homegit clone https://github.com/chicagomg/start.git start
+    cd ~ && git clone https://github.com/chicagomg/start.git start
     cd start
     sudo ./start.sh
 fi
