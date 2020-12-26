66f1f4661dd17f41a6f7e9ef0a8a9d3cd0a67d5c
Alexey Ryabtsev <61757573+chicagomg@users.noreply.github.com>
2020-12-26 03:05:38 +0300

    Update button.sh

--- button.sh
+++ button.sh
@@ -2,13 +2,14 @@
 
 #Проверяем установлен ли гит
 if [ "$(git version | cut -c 1-11)" != "git version" ];then
-    sudo apt-get install git
+    sudo apt update
+    sudo apt install git -y
 fi
 
-read -p "Press enter to continue"
+#read -p "Press enter to continue"
 
 if [ "$(git version | cut -c 1-11)" = "git version" ];then
     cd ~ && git clone https://github.com/chicagomg/start.git start
     cd start
     sudo ./start.sh
-fi
+figit --version | cut -c 1-11
